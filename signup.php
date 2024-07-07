85a64e7e308b1bcaad0170965427ff69f1bb3f1e
devosema <benaliosama3@gmail.com>
2024-07-07 17:59:01 +0100

    add favicon

--- signup.php
+++ signup.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./assets//favicon-32x32.png">
     <title>Address Book - Sign up</title>
     <link rel="stylesheet" href="./css/main.css" />
 </head>
